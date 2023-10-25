b4bb3cc0f1fb31b7bf71baea6e59cee1d3d5fdbb
Nayid4 <nayid2004@gmail.com>
2023-10-24 20:38:49 -0500

    cambio en edit_profile

--- lib/ui/pages/Profile/edit_profile.dart
+++ lib/ui/pages/Profile/edit_profile.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_diccionario/ui/widgets/Buttoms/CustomElevationButtom.dart';
-import 'package:frontend_diccionario/ui/Widgets/Navbar/navbar.dart';
 import 'package:frontend_diccionario/ui/config/theme/app_theme.dart';
 import 'package:frontend_diccionario/ui/Widgets/TextFormField/CustomTextfield.dart';
+import 'package:frontend_diccionario/ui/widgets/Buttoms/CustomElevationButtom.dart';
+import 'package:frontend_diccionario/ui/widgets/Logo/logo.dart';
+import 'package:get/get.dart';
 
 class EditProfile extends StatelessWidget {
   const EditProfile({super.key});
@@ -11,96 +12,100 @@ class EditProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     AppTheme theme = AppTheme();
     double screenWidth = MediaQuery.of(context).size.width;
-    final lista = ["Nuevo Nombre",
-    "Nuevo Email",
-    "Nuevo Telefono",
-    "Descripcion"
+    final lista = [
+      "Nuevo Nombre",
+      "Nuevo Email",
+      "Nuevo Telefono",
+      "Descripcion"
     ];
 
     return Scaffold(
-      body: Container(
-        color: theme.color("primary"),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 70,
-              width: screenWidth,
-              child: Image.asset("assets/Logo.jpeg", fit: BoxFit.contain),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                width: screenWidth * 0.9,
-                margin: const EdgeInsets.only(bottom: 20),
-                decoration: BoxDecoration(
-                  color: theme.color("fourth"),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: ListView(
-                  children: [
-                    SizedBox(
-                      width: screenWidth * 0.8,
-                      child: const Text(
-                        'Editar Perfil',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color(0xFFE6C068),
-                          fontSize: 30,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                        'Por favor ingrese los datos solicitados\npara editar su perfil',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w700,
-                            height: 0,
-                        ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    for (var item in lista)
-                      Column(
-                        children: [
-                          item == 'Descripcion' ? 
-                          CustomTextFormField(labelText: item, lineas: 3,)
-                          : 
-                          CustomTextFormField(labelText: item),
-                          
-                          const SizedBox(
-                            height: 10,
-                          ),
-                        ],
-                      ),
-
-                    const CustomElevatedButton(buttonText: "Guardar Cambios"),
-                    
-                  ],
+      backgroundColor: theme.color("primary"),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: IconButton(
+                  onPressed: () {
+                    Get.toNamed('/profile');
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
                 ),
               ),
+              const Center(child: Logo()),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              width: screenWidth * 0.9,
+              margin: const EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                color: theme.color("fourth"),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: ListView(
+                children: [
+                  SizedBox(
+                    width: screenWidth * 0.8,
+                    child: const Text(
+                      'Editar Perfil',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFFE6C068),
+                        fontSize: 30,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Por favor ingrese los datos solicitados\npara editar su perfil',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  for (var item in lista)
+                    Column(
+                      children: [
+                        item == 'Descripcion'
+                            ? CustomTextFormField(
+                                labelText: item,
+                                lineas: 3,
+                              )
+                            : CustomTextFormField(labelText: item),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  const CustomElevatedButton(buttonText: "Guardar Cambios"),
+                ],
+              ),
             ),
-            const SizedBox(
-              height: 30,
-            )
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 30,
+          )
+        ],
       ),
-      bottomNavigationBar: const NavBarCategory(),
     );
   }
 }
