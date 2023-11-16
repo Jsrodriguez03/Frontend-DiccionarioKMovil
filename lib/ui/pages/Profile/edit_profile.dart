11c7af093e10cdd3f117c132004fbebfb2e5a803
Santiago <juniorsantiagoalvarez03@gmail.com>
2023-11-15 20:45:45 -0500

    Solucionando Responsive de Vistas Editar Perfil y Agregar Palabras

--- lib/ui/pages/Profile/edit_profile.dart
+++ lib/ui/pages/Profile/edit_profile.dart
@@ -12,6 +12,7 @@ class EditProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     AppTheme theme = AppTheme();
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
 
     final lista = ["Nuevo Nombre", "Nuevo Email", "Descripcion"];
 
@@ -22,92 +23,96 @@ class EditProfile extends StatelessWidget {
       "Descripcion": TextEditingController(),
     };
 
-    return Scaffold(
-      backgroundColor: theme.color("primary"),
-      body: Stack(
-        children: [
-          //Flecha
-          const Positioned(
-            top: 0,
-            left: 0,
-            child: Flecha(navigation: "/profile"),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Logo(heigth: screenWidth * 0.25),
-              const SizedBox(height: 10),
-              Container(
-                padding: const EdgeInsets.all(10),
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                constraints: const BoxConstraints(maxHeight: 500),
-                width: screenWidth * 0.9,
-                child: Expanded(
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: screenWidth * 0.8,
-                        child: const Text(
-                          'Editar Perfil',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0xFFE6C068),
-                            fontSize: 40,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Por favor ingrese los datos solicitados\npara editar su perfil',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w700,
-                          height: 0,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      for (var item in lista)
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            item == 'Descripcion'
-                                ? CustomTextFormField(
-                                    labelText: item,
-                                    lineas: 3,
-                                    controller: controllers[item] ??
-                                        TextEditingController(),
-                                  )
-                                : CustomTextFormField(
-                                    labelText: item,
-                                    controller: controllers[item] ??
-                                        TextEditingController(),
-                                  ),
-                            const SizedBox(height: 10),
-                          ],
-                        ),
-                      CustomElevatedButton(
-                        buttonText: "Guardar Cambios",
-                        onPressed: () {
-                          // Usa los controladores para acceder a la información del formulario.
-                          for (var controller in controllers.entries) {
-                            print(
-                                '${controller.key}: ${controller.value.text}');
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: theme.color("primary"),
+        body: ListView(
+          children: [
+            Stack(
+              children: [
+                const Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Flecha(navigation: "/profile"),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 30)
-        ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: screenHeight * 0.1),
+                    Logo(heigth: screenWidth * 0.25),
+                    const SizedBox(height: 10),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.symmetric(horizontal: 20),
+                      constraints: const BoxConstraints(maxHeight: 500),
+                      width: screenWidth * 0.9,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.8,
+                            child: const Text(
+                              'Editar Perfil',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xFFE6C068),
+                                fontSize: 40,
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Por favor ingrese los datos solicitados\npara editar su perfil',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w700,
+                              height: 0,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          for (var item in lista)
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                item == 'Descripcion'
+                                    ? CustomTextFormField(
+                                        labelText: item,
+                                        lineas: 3,
+                                        controller: controllers[item] ??
+                                            TextEditingController(),
+                                      )
+                                    : CustomTextFormField(
+                                        labelText: item,
+                                        controller: controllers[item] ??
+                                            TextEditingController(),
+                                      ),
+                                const SizedBox(height: 10),
+                              ],
+                            ),
+                          CustomElevatedButton(
+                            buttonText: "Guardar Cambios",
+                            onPressed: () {
+                              // Usa los controladores para acceder a la información del formulario.
+                              for (var controller in controllers.entries) {
+                                print(
+                                    '${controller.key}: ${controller.value.text}');
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 30)
+          ],
+        ),
       ),
     );
   }
